81f0bb8eaa42f6a464fbe35cda23716113539fdb
qbnyp <moneycloudy@naver.com>
2024-05-31 01:57:19 +0900

    메인페이지 Fe 시간,온도 api 데이터 반영

--- app/src/main/java/com/example/examproject/MainActivity.java
+++ app/src/main/java/com/example/examproject/MainActivity.java
@@ -5,16 +5,19 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -26,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "ApiExplorer";
     private Handler handler = new Handler();
+    TextView mwTime_7,mwTime_6,mwTime_5,mwTime_4,mwTime_3,mwTime_2;
+    long mNow;
+    String date;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat tFormat = new SimpleDateFormat("HH:mm");
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREAN);
 
     private Runnable updateTimeRunnable = new Runnable() {
         @Override
@@ -52,15 +61,15 @@ public class MainActivity extends AppCompatActivity {
         new ApiExplorerTask().execute();
     }
 
-    long mNow;
-    Date mDate;
-    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
     private String getTime(){
-        mNow = System.currentTimeMillis();
-        mDate = new Date(mNow);
+        //날짜 가져오는 함수
 
-        return mFormat.format(mDate);
+        mNow = System.currentTimeMillis();
+        Date mDate = new Date(mNow);
+
+        date = mFormat.format(mDate);
+
+        return date;
     }
 
     private String getCurrentTime() {
@@ -68,8 +77,8 @@ public class MainActivity extends AppCompatActivity {
         TimeZone timeZone = TimeZone.getDefault();
         calendar.setTimeZone(timeZone);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREAN);
         dateFormat.setTimeZone(timeZone);
+
         return dateFormat.format(calendar.getTime());
     }
 
@@ -82,8 +91,30 @@ public class MainActivity extends AppCompatActivity {
 
     // AsyncTask 클래스
     private class ApiExplorerTask extends AsyncTask<Void, Void, String> {
+
         @Override
         protected String doInBackground(Void... voids) {
+            StringBuilder result = null; //HTTP 응답 데이터 받을공간
+            try {
+                result = new StringBuilder(fetchData_Api());
+            } catch (Exception e) {
+                Log.e(TAG, "Error: ", e);
+            }
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                try {
+                    parseDataAndUpdateUI(new StringBuilder(result));
+                } catch (JSONException | ParseException e) {
+                    Log.e(TAG, "Error: ", e);
+                }
+            }
+        }
+
+        protected String fetchData_Api() throws IOException, JSONException {
             StringBuilder result = new StringBuilder();
             try {
                 StringBuilder urlBuilder = new StringBuilder("https://api.openweathermap.org/data/2.5/forecast");
@@ -109,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
                     while ((line = rd.readLine()) != null) {
                         result.append(line); // API 응답을 result StringBuilder에 추가
                     }
-                    rd.close();
                 } else {
                     // 실패 시 errorstream
                     rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
@@ -118,47 +148,82 @@ public class MainActivity extends AppCompatActivity {
                         // 에러 응답을 읽어 처리
                         Log.e(TAG, "Error Response: " + line);
                     }
-                    rd.close();
                 }
-
-                // JSON 파싱
-                JSONObject response = new JSONObject(result.toString());
-                JSONArray list = response.getJSONArray("list");
-
-                for (int i = 2; i < list.length(); i++) { //i = 0은 6시간전이라 필요없음 3시간전까지만 유효 3시일때 전날 21시 데이터 받는거 방지
-                    JSONObject item = list.getJSONObject(i);
-                    Log.d(TAG, "item: " + item.toString());
-
-                    String dtTxt = item.getString("dt_txt");
-
-                    JSONObject main = item.getJSONObject("main");
-                    String temp = main.getString("temp"); //기온
-                    String humidity = main.getString("humidity"); //습도
-
-                    JSONArray weatherArray = item.getJSONArray("weather");
-                    JSONObject weather = weatherArray.getJSONObject(0);
-                    String description = weather.getString("description"); //날씨 상태
-
-                    double pop = item.getDouble("pop");
-                    double rainVolume = 0;
-                    if (item.has("rain")) { //아이템이 있을때도 없을때도 있음
-                        JSONObject rain = item.getJSONObject("rain");
-                        if (rain.has("3h")) { //3시간동안의 강수량을 보여주기에 3h로 해줘야 가져올수 있음
-                            rainVolume = rain.getDouble("3h");
-                        }
-                    }
-
-                    Log.d(TAG, "weather dxTxt = " + dtTxt);
-                    Log.d(TAG, "weather temp = " + temp);
-                    Log.d(TAG, "weather humidity = " + humidity);
-                    Log.d(TAG, "weather description = " + description);
-                    Log.d(TAG, "weather pop = " + pop);
-                }
+                rd.close();
                 conn.disconnect();
             } catch (Exception e) {
                 Log.e(TAG, "Error: ", e);
             }
             return result.toString();
         }
+
+        private void parseDataAndUpdateUI(StringBuilder result) throws JSONException, ParseException {
+            JSONObject response = new JSONObject(result.toString());
+            JSONArray list = response.getJSONArray("list");
+
+            for (int i = 2; i < list.length(); i++) { //i = 0은 6시간전이라 필요없음 3시간전까지만 유효 3시일때 전날 21시 데이터 받는거 방지
+
+                JSONObject item = list.getJSONObject(i); //날씨 item 오브젝트로 받아옴
+                Date dtTxt_date = dateFormat.parse(item.getString("dt_txt"));
+                String dTxt = mFormat.format(dtTxt_date); //날짜만 yy:mm:dd
+                String tTxt = tFormat.format(dtTxt_date); //시간만 hh:mm:ss
+                JSONObject main = item.getJSONObject("main");
+
+                Log.d(TAG , "dtTxt: " + dTxt + "date :" + date);
+                Log.d(TAG, "item: " + item.toString());
+
+                String double_temp = main.getString("temp"); //기온
+                int dotIndex = double_temp.indexOf('.');
+                String temp = double_temp.substring(0, dotIndex) + "℃";
+
+
+                String humidity = main.getString("humidity"); //습도
+
+                JSONArray weatherArray = item.getJSONArray("weather");
+                JSONObject weather = weatherArray.getJSONObject(0);
+                String description = weather.getString("description"); //날씨 상태
+
+                double pop = item.getDouble("pop");
+                double rainVolume = 0;
+                if (item.has("rain")) { //아이템이 있을때도 없을때도 있음
+                    JSONObject rain = item.getJSONObject("rain");
+                    if (rain.has("3h")) { //3시간동안의 강수량을 보여주기에 3h로 해줘야 가져올수 있음
+                        rainVolume = rain.getDouble("3h");
+                    }
+                }
+
+                if(i <= 7){ //메인페이지 고정으로 출력해주기 6개까지 해줘야함
+                    // 동적으로 ID 생성
+                    String TimeV_Id = "mwTime_" + i;
+                    int Time_resId = getResources().getIdentifier(TimeV_Id , "id", getPackageName());
+
+                    String TempV_Id = "mwTemp_" + i;
+                    int Temp_resId = getResources().getIdentifier(TempV_Id, "id", getPackageName());
+
+                    // 해당 ID의 TextView 찾기
+                    TextView mwTime = findViewById(Time_resId);
+                    TextView mwTemp = findViewById(Temp_resId);
+
+                    Log.d(TAG, "mwTime" + mwTime + "tTxt" + tTxt);
+
+                    if (mwTime != null) {
+                        // TextView 설정 (예: 텍스트 설정)
+                        mwTime.setText(tTxt);
+                        mwTemp.setText(temp);
+                        // 디버그 로그 출력
+                        Log.d("TextViewSetup", "설정된 TextView ID: " + Time_resId);
+                    } else {
+                        // 디버그 로그 출력
+                        Log.e("TextViewSetup", "TextView를 찾을 수 없음: " + Time_resId);
+                    }
+                }
+////              Log.d(TAG, "weather dxTxt = " + dtTxt);
+//                Log.d(TAG, "weather temp = " + temp);
+//                Log.d(TAG, "weather humidity = " + humidity);
+//                Log.d(TAG, "weather description = " + description);
+//                Log.d(TAG, "weather pop = " + pop);
+            }
+        }
+
     }
 }
