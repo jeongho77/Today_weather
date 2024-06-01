f4fb52c6672199da62489af1ad2cc8c9f06e7e2c
qbnyp <moneycloudy@naver.com>
2024-06-01 23:29:21 +0900

    날씨 리스트 액티비티 FE 임시 완성

---
+++ app/src/main/java/com/example/examproject/NextWeatherActivity.java
@@ -0,0 +1,26 @@
+package com.example.examproject;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.examproject.databinding.ActivityNextWeatherBinding;
+
+public class NextWeatherActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+private ActivityNextWeatherBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
